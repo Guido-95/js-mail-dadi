60064af996ea6e0f43c71c5507e9d3eb4a11f160
Guido-95 <guidocarna1995@gmail.com>
2021-04-28 15:28:35 +0200

    esercizio email

---
+++ js/email.js
@@ -0,0 +1,30 @@
+// Chiedi all'utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull'esito del controllo.
+
+// richiesta email
+var emailUtente = prompt("Inserisci email");
+
+console.log(emailUtente);
+
+// array email
+var listaEmail = ["email1","email2","email3",
+"email4","email5"];
+console.log(listaEmail);
+
+var chiaveAccesso = false;
+for (var i = 0; i < listaEmail.length; i++) {
+    
+    if (emailUtente == listaEmail[i]) {
+        chiaveAccesso = true;  
+    } 
+
+}
+
+if (chiaveAccesso == true) {
+    document.getElementById("log-success").innerHTML = "Complimenti, sei loggato come: " + emailUtente;
+    
+} else {
+    document.getElementById("log-fail").innerHTML = "accesso fallito";
+}
+
